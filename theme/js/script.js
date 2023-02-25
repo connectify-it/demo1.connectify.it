452f044620be8ac27f9379f0bd20cdbd938c0baf
Alberto Gonella <albygonella@gmail.com>
2023-02-25 18:09:19 +0100

    responsive is working 🎉

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,11 +1,24 @@
 window.onload = () => {
-  getProducts();
+  createGrid();
 };
+
+async function createGrid() {
+  let products = await getProducts();
+  let container = document.getElementById("productsContainer");
+  products.forEach((item) => {
+    container.innerHTML += `
+      <div class="card text-bg-dark product">
+         <img src="${item.url}" class="card-img-top" alt="${item.name}">
+         <div class="card-body">
+            <p class="card-text">${item.name}</p>
+         </div>
+      </div>
+      `;
+  });
+}
 
 async function getProducts() {
   let data = await fetch("../products.json");
   data = await data.json();
-  console.log(await data);
+  return await data;
 }
-
-function createGrid() {}
